cd533fc4992f25add614b6f7e99867d641fc6e45
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-11-18 10:19:48 -0800

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  let splitted = timeString.split(' ')
+  let readyParsed = parseInt(splitted)
+  if (readyParsed < 12) {
+    return "Good Morning";
+  } else if (readyParsed < 17 && readyParsed > 12) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(greetWord) {
+  document.getElementById("greeting").innerText = greetWord
+}
