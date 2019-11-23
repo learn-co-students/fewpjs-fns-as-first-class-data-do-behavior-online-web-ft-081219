d4f53de531831830be57e14a747000a77e658264
KHong <albtrossfacefall@gmail.com>
2019-11-22 19:31:08 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  const hour_str = string.split(":")[0];
+  const hour = parseInt(hour_str);
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour <= 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById('greeting').innerText = string
+}
