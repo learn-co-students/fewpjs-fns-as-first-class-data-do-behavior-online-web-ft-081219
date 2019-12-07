3548a3dd49788d46c5641e49fc241005c92dbd2d
Emilia <emilia.mena26@gmail.com>
2019-12-07 15:23:31 -0500

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 function greet(timeString) {
-    const time = parseInt(timeString)
+    const time = parseInt(timeString);
     if (time < 12) return "Good Morning";
     if (time <= 17 && time >= 12) return "Good Afternoon";
     return "Good Evening";
